3dbd5408a3b85c3b0256a09020620191f54aaa82
Felix <barosiofelix@gmail.com>
2022-11-21 16:46:18 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: [ "Fiction", "Non-Fiction" ] 
+    validate :clickbait?
+
+    ALLOWED = [
+        /Won't Believe/i,
+        /New/i,
+        /Nice \d/i,
+        /Post /i
+    ]
+
+
+    def clickbait?
+        if ALLOWED.none? { |click| click.match title}
+            errors.add(:title, "invalid title")
+        end
+    end
+    
 end
